4824d252be3a0a8def824a6e1289648a9437beed
vijayadla <vijayadla7071@gmail.com>
2025-03-12 07:44:39 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function clickAll(){
+    var a=document.getElementById("content-visible")
+    a.style.display="block"
+    // location.reload();
+}
+
+function FreshVegetables(){
+    var a=document.getElementById("content-visible1")
+    a.style.display="block"
+    location.reload();
+}
